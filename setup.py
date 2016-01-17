ad9c8348878862a8369e87e350b1ca6cc947e31f
James Tauber <jtauber@jtauber.com>
2016-01-16 20:32:37 -0500

    travis / flake8 / setup.py boilerplate

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="greek-utils",
+    version="0.1",
+    description="various utilities for processing Ancient Greek",
+    license="MIT",
+    url="http://github.com/jtauber/greek-utils",
+    author="James Tauber",
+    author_email="jtauber@jtauber.com",
+    packages=["greekutils"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Text Processing",
+        "Topic :: Text Processing :: Linguistic",
+        "Topic :: Utilities",
+    ],
+)
