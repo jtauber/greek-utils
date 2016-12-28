31369fbe56fa638fd76f3974334c80494108b70b
James Tauber <jtauber@jtauber.com>
2016-12-28 09:17:51 +0800

    added Python 3.6

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Text Processing",
         "Topic :: Text Processing :: Linguistic",
         "Topic :: Utilities",
